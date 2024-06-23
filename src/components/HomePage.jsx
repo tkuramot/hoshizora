b9c2becedcb42e4ead1517cd71de1eff14268b8c
Ishi <syi378039@gmail.com>
2024-06-23 11:54:40 +0900

    ホーム画面

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,25 +1,64 @@
 import React from "react";
-import Grid from "@mui/material/Grid";
-import Container from "@mui/material/Container";
-import RangeSlider from "./RangeSlider";
-import Card from "./Card";
-import ContentFetcher from "./ContentFetcher";
 
-export default function HomePage() {
+const HomePage = () => {
+  const backgroundStyle = {
+    position: "relative",
+    width: "100vw",
+    height: "100vh",
+    overflow: "hidden",
+  };
+
+  const backgroundImageStyle = {
+    position: "absolute",
+    top: 0,
+    left: 0,
+    width: "100%",
+    height: "100%",
+    backgroundImage: "url(src/assets/background.jpg)",
+    backgroundSize: "cover",
+    backgroundPosition: "center",
+    zIndex: 1,
+  };
+
+  const overlayStyle = {
+    position: "absolute",
+    top: 0,
+    width: "100%",
+    height: "100%",
+    backgroundColor: "rgba(64, 0, 0, 0.1)",
+    zIndex: 2,
+  };
+
+  const contentStyle = {
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+    zIndex: 3,
+    color: "rgba(247, 233, 233)",
+    textAlign: "center",
+    writingMode: "vertical-rl",
+    fontSize: "2rem",
+    fontWeight: "bold",
+    backgroundStyle: "rgba(64, 0, 0, 0.1)",
+  };
+
+  // const na = {
+  //   position: "absolute",
+  //   top: 0,
+  //   width: "100%",
+  //   height: "100%",
+  //   backgroundColor: "rgba(64, 0, 0, 0.1)",
+  //   zIndex: 2,
+  // };
+
   return (
-    <Container>
-      <Grid container spacing={12}>
-        <Grid item xs={4}>
-          <RangeSlider />
-        </Grid>
-        <Grid item xs={4}>
-          <Card />
-        </Grid>
-        <Grid item xs={4}>
-          <Card />
-        </Grid>
-      </Grid>
-      <ContentFetcher />
-    </Container>
+    <div style={backgroundStyle}>
+      <div style={backgroundImageStyle}></div>
+      <div style={overlayStyle}></div>
+      <div style={contentStyle}>こ は る さ ん の 本 棚</div>
+    </div>
   );
-}
+};
+
+export default HomePage;
